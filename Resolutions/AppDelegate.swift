a19d160cf24766182a585f16d61742d99f84e9b9
Daniel Ma <drailskid@yahoo.com>
2016-12-19 14:52:25 -0800

    Initial Commit

---
+++ Resolutions/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Resolutions
+//
+//  Created by Daniel Ma on 12/19/16.
+//  Copyright © 2016 Daniel Ma. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ Resolutions/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Resolutions
+//
+//  Created by Daniel Ma on 12/19/16.
+//  Copyright © 2016 Daniel Ma. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
